cc0ce4bec01aaaac929b13ce27557ea4f0cbe926
Christianrslv <christianrslv22@gmail.com>
2023-02-13 14:41:56 -0300

    Add global css

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+import { defaulTheme } from './../styles/themes/default';
+import 'styled-components';
+import { defaulTheme } from '../styles/themes/default'; 
+
+type ThemeType = typeof defaulTheme;
+
+declare module 'styled-components' {
+    export interface DefaulTheme extends ThemeType {}
+}
